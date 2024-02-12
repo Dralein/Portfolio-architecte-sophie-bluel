d1c758e20fa8752729bd5fbfd20fd8e44d896d1b
Dralein <enzo.ghaz@gmail.com>
2024-02-12 11:10:51 +0100

    1.0

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,10 +1,10 @@
 /* Affichage de la gallery en JS */
 async function getWorks() {
   try {
-    const response = await fetch('http://localhost:5678/api/works');
+    const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
   } catch (error) {
-    console.error('Erreur lors de la récupération des travaux :', error);
+    console.error("Erreur lors de la récupération des travaux :", error);
     return [];
   }
 }
@@ -15,9 +15,9 @@ async function displayWorks() {
 
     const gallery = document.querySelector("#js-gallery");
     works.forEach((work) => {
-      const figure = document.createElement('figure');
-      const img = document.createElement('img');
-      const figcaption = document.createElement('figcaption');
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
 
       img.src = work.imageUrl;
       img.alt = work.title;
@@ -28,7 +28,7 @@ async function displayWorks() {
       gallery.appendChild(figure);
     });
   } catch (error) {
-    console.error('Erreur lors de l\'affichage des travaux :', error);
+    console.error("Erreur lors de l'affichage des travaux :", error);
   }
 }
 
@@ -38,7 +38,7 @@ displayWorks();
 
 async function displayCategorysButtons() {
   try {
-    const response = await fetch('http://localhost:5678/api/categories');
+    const response = await fetch("http://localhost:5678/api/categories");
     const categorys = await response.json();
 
     const filters = document.querySelector(".filters");
@@ -49,12 +49,11 @@ async function displayCategorysButtons() {
       filters.appendChild(btn);
     });
   } catch (error) {
-    console.error('Erreur :', error);
+    console.error("Erreur :", error);
   }
 }
 
 displayCategorysButtons();
-
 
 async function filterCategory() {
   const works = await getWorks();
@@ -80,9 +79,9 @@ async function filterCategory() {
 function displayFilteredProjects(filteredWorks) {
   const gallery = document.querySelector("#js-gallery");
   filteredWorks.forEach((work) => {
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    const figcaption = document.createElement('figcaption');
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
 
     img.src = work.imageUrl;
     img.alt = work.title;
@@ -98,34 +97,34 @@ filterCategory();
 document.addEventListener("DOMContentLoaded", () => {
   const logged = window.sessionStorage.logged;
   const admin = document.querySelector(".admin");
-  const edit = document.getElementById('edit1');
+  const edit = document.getElementById("edit1");
   const logout = document.querySelector("header nav .logout");
   const modalContent = document.querySelector(".modal");
   const galleryModal = document.querySelector(".modalinterior");
   const xmark = document.querySelector(".modal .fa-xmark");
-  const addImg = document.getElementById("addimg")
-  const leftmark = document.getElementById('leftarrow')
-  const modality2 = document.querySelector(".modal2")
-  const iconmark2 = document.getElementById("iconmark2")
+  const addImg = document.getElementById("addimg");
+  const leftmark = document.getElementById("leftarrow");
+  const modality2 = document.querySelector(".modal2");
+  const iconmark2 = document.getElementById("iconmark2");
   const imageUploadInput = document.getElementById("imageUploadInput");
   const titleInput = document.getElementById("titleInput");
   const categoryIdInput = document.getElementById("categoryIdInput");
   const addImgButton = document.getElementById("addimg1");
 
   if (logged === "true") {
-    edit.style.opacity = '1';
-    const iconElement = document.createElement('i');
+    edit.style.opacity = "1";
+    const iconElement = document.createElement("i");
     iconElement.classList.add("fa-solid", "fa-pen-to-square");
     admin.appendChild(iconElement);
-    admin.appendChild(document.createTextNode(' '));
-    const textElement = document.createElement('span');
+    admin.appendChild(document.createTextNode(" "));
+    const textElement = document.createElement("span");
     textElement.textContent = "modifier";
     admin.appendChild(textElement);
     logout.textContent = "logout";
     logout.addEventListener("click", () => {
-        window.sessionStorage.logged = false;
-        window.sessionStorage.token = false;
-        location.reload(true);
+      window.sessionStorage.logged = false;
+      window.sessionStorage.token = false;
+      location.reload(true);
     });
   }
 
@@ -135,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   iconmark2.addEventListener("click", () => {
     modality2.style.display = "none";
-    modalContent.style.display = "none"; 
+    modalContent.style.display = "none";
   });
 
   xmark.addEventListener("click", () => {
@@ -161,7 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const span = document.createElement("span");
       const trash = document.createElement("i");
       trash.classList.add("fa-solid", "fa-trash-can");
-      span.classList.add("trashicon")
+      span.classList.add("trashicon");
       trash.id = galleryItem.id;
       img.src = galleryItem.imageUrl;
       span.appendChild(trash);
@@ -169,20 +168,20 @@ document.addEventListener("DOMContentLoaded", () => {
       figure.appendChild(img);
       galleryModal.appendChild(figure);
     });
-  
+
     function deleteGallery() {
       const deleteTrash = document.querySelectorAll(".fa-trash-can");
       deleteTrash.forEach((trash) => {
         trash.addEventListener("click", (e) => {
           const id = trash.id;
-          const token = sessionStorage.getItem('token');
-    
+          const token = sessionStorage.getItem("token");
+
           const init = {
             method: "DELETE",
             headers: {
               "Content-Type": "application/json",
-              "Authorization": `Bearer ${token}`
-            }
+              Authorization: `Bearer ${token}`,
+            },
           };
           fetch(`http://localhost:5678/api/works/${id}`, init)
             .then((response) => {
@@ -213,65 +212,90 @@ document.addEventListener("DOMContentLoaded", () => {
   leftmark.addEventListener("click", () => {
     modalContent.style.display = "flex";
     modality2.style.display = "none";
-    
   });
-
 
   function isFormValid() {
     const imageInput = document.getElementById("imageUploadInput");
     const titleInput = document.getElementById("titleInput");
     const categoryInput = document.getElementById("categoryIdInput");
-  
-    
-    const isFilled = imageInput.files.length > 0 && titleInput.value.trim() !== '' && categoryInput.value.trim() !== '';
-  
+
+    const isFilled =
+      imageInput.files.length > 0 &&
+      titleInput.value.trim() !== "" &&
+      categoryInput.value.trim() !== "";
+
     return isFilled;
   }
-  
+
   function updateButtonState() {
     const isFormReady = isFormValid();
-  
-    
+
     addImgButton.style.backgroundColor = isFormReady ? "#FFFFFF" : "#A7A7A7";
     addImgButton.style.borderColor = isFormReady ? "#1D6154" : "#A7A7A7";
     addImgButton.style.color = isFormReady ? "#1D6154" : "#FFFFFF";
-    addImgButton.disabled = !isFormReady; 
+    addImgButton.disabled = !isFormReady;
   }
-  
- 
-  document.getElementById("imageUploadInput").addEventListener("change", updateButtonState);
-  document.getElementById("titleInput").addEventListener("input", updateButtonState);
-  document.getElementById("categoryIdInput").addEventListener("input", updateButtonState);
 
-  function addGallery() {
+  document
+    .getElementById("imageUploadInput")
+    .addEventListener("change", updateButtonState);
+  document
+    .getElementById("titleInput")
+    .addEventListener("input", updateButtonState);
+  document
+    .getElementById("categoryIdInput")
+    .addEventListener("input", updateButtonState);
+
+  async function addGallery() {
     const imageUploadForm = document.getElementById("imageUploadForm");
 
     imageUploadForm.addEventListener("submit", async (e) => {
       e.preventDefault();
-    
+
       const formData = new FormData();
-      formData.append('image', document.getElementById("imageUploadInput").files[0]);
-      formData.append('title', document.getElementById("titleInput").value);
-      formData.append('category', document.getElementById("categoryIdInput").value);
-    
+      formData.append(
+        "image",
+        document.getElementById("imageUploadInput").files[0]
+      );
+      formData.append("title", document.getElementById("titleInput").value);
+      formData.append(
+        "category",
+        document.getElementById("categoryIdInput").value
+      );
+
       const token = window.sessionStorage.getItem("token");
-    
+
       const options = {
         method: "POST",
         headers: {
-          'Accept': 'application/json',
-          "Authorization": `Bearer ${token}`
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
         },
         body: formData,
       };
 
-      console.log(formData);
-    
       try {
-        const response = await fetch("http://localhost:5678/api/works", options);
-    
+        const response = await fetch(
+          "http://localhost:5678/api/works",
+          options
+        );
+
         if (response.ok) {
           console.log("Image envoyée avec succès !");
+
+          const responseData = await response.json();
+
+          // Ajouter l'image directement au DOM
+          const newImage = document.createElement("img");
+          newImage.src = responseData.imageUrl;
+
+          const gallery = document.querySelector("#js-gallery");
+          const figure = document.createElement("figure");
+          figure.appendChild(newImage);
+          gallery.appendChild(figure);
+
+          // Rafraîchir la galerie pour afficher la nouvelle image
+          await displayWorks();
         } else {
           console.error("Erreur lors de l'envoi de l'image.");
         }
@@ -280,25 +304,23 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
-  
+
   addGallery();
 
-  const editDiv = document.getElementById('edit1');
+  const editDiv = document.getElementById("edit1");
   if (logged === "false") {
     editDiv.parentNode.removeChild(editDiv);
   }
-
-
-
 });
 
 function previewImage(input) {
   if (input.files && input.files[0]) {
-     var reader = new FileReader();
+    var reader = new FileReader();
     reader.onload = function (e) {
-     document.getElementById('preview').innerHTML = '<img src="' + e.target.result + '">';
+      document.getElementById("preview").innerHTML =
+        '<img src="' + e.target.result + '">';
+      document.getElementById("modalPreview").style.display = "none";
     };
     reader.readAsDataURL(input.files[0]);
   }
-
 }
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,7 +1,7 @@
 const email = document.querySelector("form #email");
 const password = document.querySelector("form #password");
 const form = document.querySelector("form");
-const messageErreur = document.querySelector(".login p");
+const messageErreur = document.getElementById("messageErreur");
 
 async function login() {
   form.addEventListener("submit", async (e) => {
@@ -11,37 +11,39 @@ async function login() {
     const userPwd = password.value;
 
     try {
-      const response = await fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           email: userEmail,
-          password: userPwd
-        })
+          password: userPwd,
+        }),
       });
 
       if (response.ok) {
         const user = await response.json();
 
         if (user.token) {
-          window.sessionStorage.setItem('token', user.token);
+          window.sessionStorage.setItem("token", user.token);
           window.sessionStorage.logged = true;
           window.location.href = "index.html";
         } else {
           email.classList.add("inputErrorLogin");
           password.classList.add("inputErrorLogin");
-          messageErreur.textContent = "Votre email ou votre mot de passe est incorrect";
+          messageErreur.textContent =
+            "Votre email ou votre mot de passe est incorrect";
         }
       } else {
-        throw new Error('Erreur de connexion');
+        throw new Error("Erreur de connexion");
       }
     } catch (error) {
-      console.error('Erreur :', error);
+      console.error("Erreur :", error);
       email.classList.add("inputErrorLogin");
       password.classList.add("inputErrorLogin");
-      messageErreur.textContent = "Une erreur s'est produite lors de la connexion.";
+      messageErreur.textContent =
+        "Une erreur s'est produite lors de la connexion.";
     }
   });
 }
