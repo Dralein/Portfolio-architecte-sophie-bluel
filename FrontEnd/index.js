aae292f7dd2006a7b8982f6c29d20307908a76e0
Dralein <enzo.ghaz@gmail.com>
2024-02-25 11:18:45 +0100

    1.0

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -82,9 +82,9 @@ async function filterCategory() {
   });
 }
 
-function displayFilteredProjects(filteredWorks) {
+async function displayFilteredProjects(filteredWorks) {
   const gallery = document.querySelector("#js-gallery");
-  filteredWorks.forEach((work) => {
+  filteredWorks.forEach(async(work) => {
     const figure = document.createElement("figure");
     const img = document.createElement("img");
     const figcaption = document.createElement("figcaption");
@@ -100,252 +100,237 @@ function displayFilteredProjects(filteredWorks) {
 }
 filterCategory();
 
-document.addEventListener("DOMContentLoaded", () => {
-  const logged = window.sessionStorage.logged;
-  const admin = document.querySelector(".admin");
-  const edit = document.getElementById("edit1");
-  const logout = document.querySelector("header nav .logout");
-  const modalContent = document.querySelector(".modal");
-  const galleryModal = document.querySelector(".modalinterior");
-  const xmark = document.querySelector(".modal .fa-xmark");
-  const addImg = document.getElementById("addimg");
-  const leftmark = document.getElementById("leftarrow");
-  const modality2 = document.querySelector(".modal2");
-  const iconmark2 = document.getElementById("iconmark2");
-  const addImgButton = document.getElementById("addimg1");
+const logged = window.sessionStorage.logged;
+const admin = document.querySelector(".admin");
+const edit = document.getElementById("edit1");
+const logout = document.querySelector("header nav .logout");
+const modalContent = document.querySelector(".modal");
+const galleryModal = document.querySelector(".modalinterior");
+const xmark = document.querySelector(".modal .fa-xmark");
+const addImg = document.getElementById("addimg");
+const leftmark = document.getElementById("leftarrow");
+const modality2 = document.querySelector(".modal2");
+const iconmark2 = document.getElementById("iconmark2");
+const addImgButton = document.getElementById("addimg1");
 
-  if (logged === "true") {
-    edit.style.opacity = "1";
-    const iconElement = document.createElement("i");
-    iconElement.classList.add("fa-solid", "fa-pen-to-square");
-    admin.appendChild(iconElement);
-    admin.appendChild(document.createTextNode(" "));
-    const textElement = document.createElement("span");
-    textElement.textContent = "modifier";
-    admin.appendChild(textElement);
-    logout.textContent = "logout";
-    logout.addEventListener("click", () => {
-      window.sessionStorage.logged = false;
-      window.sessionStorage.token = false;
-      location.reload(true);
-    });
-  }
-
-  admin.addEventListener("click", () => {
-    modalContent.style.display = "flex";
+if (logged === "true") {
+  edit.style.display = "flex";
+  const iconElement = document.createElement("i");
+  iconElement.classList.add("fa-solid", "fa-pen-to-square");
+  admin.appendChild(iconElement);
+  admin.appendChild(document.createTextNode(" "));
+  const textElement = document.createElement("span");
+  textElement.textContent = "modifier";
+  admin.appendChild(textElement);
+  logout.textContent = "logout";
+  logout.addEventListener("click", () => {
+    window.sessionStorage.logged = false;
+    window.sessionStorage.token = false;
+    location.reload(true);
   });
+}
 
-  iconmark2.addEventListener("click", () => {
+admin.addEventListener("click", () => {
+  modalContent.style.display = "flex";
+});
+
+iconmark2.addEventListener("click", () => {
+  modality2.style.display = "none";
+  modalContent.style.display = "none";
+});
+
+xmark.addEventListener("click", () => {
+  modality2.style.display = "none";
+  modalContent.style.display = "none";
+});
+
+modalContent.addEventListener("click", (e) => {
+  if (e.target === modalContent) {
+    modalContent.style.display = "none";
+  }
+});
+
+modality2.addEventListener("click", (e) => {
+  if (e.target === modality2) {
     modality2.style.display = "none";
     modalContent.style.display = "none";
-  });
-
-  xmark.addEventListener("click", () => {
-    modality2.style.display = "none";
-    modalContent.style.display = "none";
-  });
-
-  modalContent.addEventListener("click", (e) => {
-    if (e.target === modalContent) {
-      modalContent.style.display = "none";
-    }
-  });
-
-  modality2.addEventListener("click", (e) => {
-    if (e.target === modality2) {
-      modality2.style.display = "none";
-      modalContent.style.display = "none";
-    }
-  });
-
-  xmark.addEventListener("click", () => {
-    modality2.style.display = "none";
-  });
-
-  async function displayGalleryModal() {
-    galleryModal.innerHTML = "";
-    const gallery = await getWorks();
-    gallery.forEach((galleryItem) => {
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-      const span = document.createElement("span");
-      const trash = document.createElement("i");
-      trash.classList.add("fa-solid", "fa-trash-can");
-      span.classList.add("trashicon");
-      trash.id = galleryItem.id;
-      img.src = galleryItem.imageUrl;
-      span.appendChild(trash);
-      figure.appendChild(span);
-      figure.appendChild(img);
-      galleryModal.appendChild(figure);
-    });
-    deleteGallery();
   }
-  displayGalleryModal();
+});
 
-  async function deleteGallery() {
-    const deleteTrash = document.querySelectorAll(".fa-trash-can");
-    deleteTrash.forEach((trash) => {
-      trash.addEventListener("click", async (e) => {
-        e.preventDefault();
+xmark.addEventListener("click", () => {
+  modality2.style.display = "none";
+});
 
-        const id = trash.id;
-        const token = sessionStorage.getItem("token");
-
-        const init = {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        };
-        try {
-          const response = await fetch(
-            `http://localhost:5678/api/works/${id}`,
-            init
-          );
-          if (response.ok) {
-            console.log("Image supprimée avec succès !");
-            const figure = trash.closest("figure");
-            if (figure) {
-              figure.remove();
-            }
-            await displayGalleryModal();
-            await displayWorks();
-          } else {
-            console.error("Erreur lors de la suppression de l'image.");
-          }
-        } catch (error) {
-          console.error("Erreur :", error);
-        }
-      });
-    });
-  }
-
-  addImg.addEventListener("click", () => {
-    modality2.style.display = "flex";
-    modalContent.style.display = "flex";
+async function displayGalleryModal() {
+  galleryModal.innerHTML = "";
+  const gallery = await getWorks();
+  gallery.forEach((galleryItem) => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const span = document.createElement("span");
+    const trash = document.createElement("i");
+    trash.classList.add("fa-solid", "fa-trash-can");
+    span.classList.add("trashicon");
+    trash.id = galleryItem.id;
+    img.src = galleryItem.imageUrl;
+    span.appendChild(trash);
+    figure.appendChild(span);
+    figure.appendChild(img);
+    galleryModal.appendChild(figure);
   });
+  deleteGallery();
+}
 
-  leftmark.addEventListener("click", () => {
-    modalContent.style.display = "flex";
-    modality2.style.display = "none";
-  });
-
-  function isFormValid() {
-    const imageInput = document.getElementById("undisplayfile");
-    const titleInput = document.getElementById("titleInput");
-
-    const isFilled =
-      imageInput.files &&
-      imageInput.files.length > 0 &&
-      titleInput.value.trim() !== "";
-
-    return isFilled;
-  }
-
-  document.getElementById("undisplayfile").addEventListener("change", () => {
-    updateFormState();
-  });
-
-  document.getElementById("titleInput").addEventListener("input", () => {
-    updateFormState();
-  });
-
-  function updateFormState() {
-    const isFormReady = isFormValid();
-
-    updateButtonState();
-  }
-
-  function updateButtonState() {
-    const isFormReady = isFormValid();
-
-    addImgButton.style.backgroundColor = isFormReady ? "#FFFFFF" : "#A7A7A7";
-    addImgButton.style.borderColor = isFormReady ? "#1D6154" : "#A7A7A7";
-    addImgButton.style.color = isFormReady ? "#1D6154" : "#FFFFFF";
-    addImgButton.disabled = !isFormReady;
-
-    if (isFormReady) {
-      addImgButton.style.cursor = "pointer";
-      addImgButton.onmouseover = function() {
-        addImgButton.style.backgroundColor = "#1D6154";
-        addImgButton.style.color = "#FFFFFF";
-    };
-     addImgButton.onmouseout = function() {
-        addImgButton.style.backgroundColor = ""; 
-        addImgButton.style.color = ""; 
-    };
-    } else {
-      addImgButton.onmouseover = null;
-      addImgButton.onmouseout = null;
-      addImgButton.style.cursor = "default";
-    }
-  }
-  updateButtonState();
-
-  async function addGallery() {
-    addImgButton.addEventListener("click", async (e) => {
+displayGalleryModal();
+async function deleteGallery() {
+  const deleteTrash = document.querySelectorAll(".fa-trash-can");
+  deleteTrash.forEach((trash) => {
+    trash.addEventListener("click", async (e) => {
       e.preventDefault();
-
-      const formData = new FormData();
-      formData.append(
-        "image",
-        document.getElementById("undisplayfile").files[0]
-      );
-      formData.append("title", document.getElementById("titleInput").value);
-      formData.append(
-        "category",
-        document.getElementById("categoryIdInput").value
-      );
-
-      const token = window.sessionStorage.getItem("token");
-
-      const options = {
-        method: "POST",
+      const id = trash.id;
+      const token = sessionStorage.getItem("token");
+      const init = {
+        method: "DELETE",
         headers: {
-          Accept: "application/json",
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: formData,
       };
-
       try {
         const response = await fetch(
-          "http://localhost:5678/api/works",
-          options
+          `http://localhost:5678/api/works/${id}`,
+          init
         );
-
         if (response.ok) {
-          console.log("Image envoyée avec succès !");
-
-          const responseData = await response.json();
-
-          const newImage = document.createElement("img");
-          newImage.src = responseData.imageUrl;
-
-          const gallery = document.querySelector("#js-gallery");
-          const figure = document.createElement("figure");
-          figure.appendChild(newImage);
-          gallery.appendChild(figure);
-
+          console.log("Image supprimée avec succès !");
+          const figure = trash.closest("figure");
+          if (figure) {
+            figure.remove();
+          }
+          await filterCategory();
           await displayGalleryModal();
           await displayWorks();
         } else {
-          console.error("Erreur lors de l'envoi de l'image.");
+          console.error("Erreur lors de la suppression de l'image.");
         }
       } catch (error) {
         console.error("Erreur :", error);
       }
     });
-  }
-  addGallery();
+  });
+}
 
-  const editDiv = document.getElementById("edit1");
-  if (logged === "false") {
-    editDiv.parentNode.removeChild(editDiv);
-  }
+addImg.addEventListener("click", () => {
+  modality2.style.display = "flex";
+  modalContent.style.display = "flex";
 });
+
+leftmark.addEventListener("click", () => {
+  modalContent.style.display = "flex";
+  modality2.style.display = "none";
+});
+
+function isFormValid() {
+  const imageInput = document.getElementById("undisplayfile");
+  const titleInput = document.getElementById("titleInput");
+  const isFilled =
+    imageInput.files &&
+    imageInput.files.length > 0 &&
+    titleInput.value.trim() !== "";
+  return isFilled;
+}
+
+document.getElementById("undisplayfile").addEventListener("change", () => {
+  updateFormState();
+});
+
+document.getElementById("titleInput").addEventListener("input", () => {
+  updateFormState();
+});
+
+function updateFormState() {
+  const isFormReady = isFormValid();
+  updateButtonState();
+}
+
+function updateButtonState() {
+  const isFormReady = isFormValid();
+  addImgButton.style.backgroundColor = isFormReady ? "#FFFFFF" : "#A7A7A7";
+  addImgButton.style.borderColor = isFormReady ? "#1D6154" : "#A7A7A7";
+  addImgButton.style.color = isFormReady ? "#1D6154" : "#FFFFFF";
+  addImgButton.disabled = !isFormReady;
+  if (isFormReady) {
+    addImgButton.style.cursor = "pointer";
+    addImgButton.onmouseover = function() {
+      addImgButton.style.backgroundColor = "#1D6154";
+      addImgButton.style.color = "#FFFFFF";
+  };
+   addImgButton.onmouseout = function() {
+      addImgButton.style.backgroundColor = ""; 
+      addImgButton.style.color = ""; 
+  };
+  } else {
+    addImgButton.onmouseover = null;
+    addImgButton.onmouseout = null;
+    addImgButton.style.cursor = "default";
+  }
+}
+
+updateButtonState();
+
+async function addGallery() {
+  addImgButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append(
+      "image",
+      document.getElementById("undisplayfile").files[0]
+    );
+    formData.append("title", document.getElementById("titleInput").value);
+    formData.append(
+      "category",
+      document.getElementById("categoryIdInput").value
+    );
+    const token = window.sessionStorage.getItem("token");
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:5678/api/works",
+        options
+      );
+      if (response.ok) {
+        console.log("Image envoyée avec succès !");
+        const responseData = await response.json();
+        const newImage = document.createElement("img");
+        newImage.src = responseData.imageUrl;
+        const gallery = document.querySelector("#js-gallery");
+        const figure = document.createElement("figure");
+        figure.appendChild(newImage);
+        gallery.appendChild(figure);
+        filterCategory();
+        displayGalleryModal();
+        displayWorks();
+      } else {
+        console.error("Erreur lors de l'envoi de l'image.");
+      }
+    } catch (error) {
+      console.error("Erreur :", error);
+    }
+  });
+}
+addGallery();
+
+const editDiv = document.getElementById("edit1");
+if (logged === "false") {
+  editDiv.parentNode.removeChild(editDiv);
+}
 
 function previewImage(input) {
   if (input.files && input.files[0]) {
